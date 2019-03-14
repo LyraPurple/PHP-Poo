c984a1f7ba47e2e8f11b6b589197971c3f6ebc68
Laugras <lew_terramp@hotmail.fr>
2019-03-14 10:56:59 +0100

    J\'appelle "Cat.php" dans mon "index.php". J\'affiche "Mon chat s\'appelle Bianca et il fait Miaou !"

--- 01_Training/index.php
+++ 01_Training/index.php
@@ -1,6 +1,18 @@
 <?php 
 
-
-
+/*  -------Adresse de le site -------
+    http://localhost/php_poo/01_Training
+    -------Adresse de le site ------- */
 require_once 'Cat.php';
+$bianca = new Cat();
+$Mina = new Cat();
 
+
+$cat = new Cat();
+// On peut affecter une valeur aux propriétés
+$cat->name = 'Bianca';
+$cat->type = 'Chat de gouttière';
+$cat->fur = 'blanc';
+
+// On peut afficher la valeur d'une propriété
+echo 'Mon chat s\'appelle ' . $cat->name . ' et il fait ' . $bianca->cry();
